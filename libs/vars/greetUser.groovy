072b1c49564e977bd7d8087f4eb6770656c2f09d
aniketpurohit <purohitaniket1@gmail.com>
2024-11-13 15:08:48 +0530

    initial commit

--- libs/vars/greetUser.groovy
+++ libs/vars/greetUser.groovy
@@ -1,4 +1,13 @@
 // greetUser.groovy
 def call(String name) {
     echo "Hello, ${name}! Welcome to the Jenkins pipeline."
+    if {false}{
+        echo "Some thing is false"
+    }
+    else if(true){
+        echo "In the else if block"
+    }
+    else{
+        echo "In the else block"
+    }
 }
