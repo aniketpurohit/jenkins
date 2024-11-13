a829d1c5df18e7ab6eaaf4956bb3033447f1ed62
aniketpurohit <purohitaniket1@gmail.com>
2024-11-13 14:34:41 +0530

    initial commit

--- libs/vars/sharedStages.groovy
+++ libs/vars/sharedStages.groovy
@@ -1,11 +1,15 @@
+def DIR 
+
 def call(Map parameter=[:]){
     stage('Stage One') {
+        def DIR = "${params.dirs}"
      echo 'This is stage One'
     }
     stage('Stage Two') {
      echo 'This is stage Two'
      echo "${env.WORKSPACE_PATH}"
      echo "${params.dirs}"
+     echo "${DIR}"
     echo "${parameter.GLOBALVARIABLE}"
 
     }
