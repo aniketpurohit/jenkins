0b1216e495baadf348b255bf1be89d63d5f4133a
aniketpurohit <purohitaniket1@gmail.com>
2024-11-19 20:58:08 +0530

    initial commit

--- libs/vars/pythondependencies.groovy
+++ libs/vars/pythondependencies.groovy
@@ -3,7 +3,7 @@
 
 def call(){
     try{
-        def output = runPythonSript("print('Something is getting done')")
+        def output = runPythonScript("print('Something is getting done')")
     }
     catch (Exception e){
         echo "failure of the python"
