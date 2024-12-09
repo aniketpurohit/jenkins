62e6868b3a93d09670c68d6cfd55e9a6fb9fefc5
aniketpurohit <purohitaniket1@gmail.com>
2024-12-09 15:13:11 +0530

    commit

--- libs/vars/sharedStages.groovy
+++ libs/vars/sharedStages.groovy
@@ -1,21 +1,27 @@
-def DIR 
+def DIR
 
 @Library('shared-lib@main') _  // Use the name you set in Jenkins configuration
 
-def call(Map parameter=[:]){
-    stage('Stage One') {
-        script{
-    DIR = "${params.dirs}"
-     echo 'This is stage One'
-
-     buildInfo()
+def call(Map parameter=[:]) {
+    pipeline {
+        agent { labels 'sc1' }
     }
-    stage('Stage Two') {
-     echo 'This is stage Two'
-     echo "${env.WORKSPACE_PATH}"
-     echo "${params.dirs}"
-     echo "${DIR}"
-    echo "${parameter.GLOBALVARIABLE}"
 
+    stage('Stage One') {
+        script {
+            DIR = "${params.dirs}"
+            echo 'This is stage One'
+
+            buildInfo()
+        }
+        stage('Stage Two') {
+            script {
+                echo 'This is stage Two'
+                echo "${env.WORKSPACE_PATH}"
+                echo "${params.dirs}"
+                echo "${DIR}"
+                echo "${parameter.GLOBALVARIABLE}"
+            }
+        }
     }
 }
