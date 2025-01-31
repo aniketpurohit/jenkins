e10d2e760c361fc6ce71a89df9f9efaaa15e2859
aniketpurohit <purohitaniket1@gmail.com>
2025-01-31 18:06:20 +0530

    update

--- libs/vars/tempWorspace.groovy
+++ libs/vars/tempWorspace.groovy
@@ -4,11 +4,15 @@ def setup(){
 
     env.TEMP_WORKSPACE = tempWorkspace
     env.WORKSPACE_PATH = tempWorkspace
+
+    sh "mkdir -p ${tempWorkspace}"
+    return tempWorkspace
 }
 
 def call(){
     def pipelineName = env.JOB_NAME.replaceAll('/','-')
     def tempWorkspace = "${env.WORKSPACE}/temp/${pipelineName}/${env.BUILD_NUMBER}"
+
     return tempWorkspace
 }
 
