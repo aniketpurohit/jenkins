f5f5f0a9ac87cef0aa04ab74e82d4fdc8b56084e
aniketpurohit <purohitaniket1@gmail.com>
2025-01-31 21:04:30 +0530

    updated

--- libs/vars/globalVarUtil.groovy
+++ libs/vars/globalVarUtil.groovy
@@ -50,7 +50,8 @@ def getRootPath(String jsonContent, Map parameters = [:]) {
     def jsonContentMap = readJSON(text: jsonContent)
     echo "Parsed JSON content: ${jsonContentMap}"
 
-    def fieldValue = jsonContentMap['ROOT_FOLDER']
+    def fieldValue = jsonContentMap.get("ROOT_FOLDER", '/var/jenkins_home/workspace/${JOB_NAME}')
+    echo "Processing field 'ROOT_FOLDER' with value '${fieldValue}'..."
 
     // Replace Jenkins environment variables
     containsKeywordList.each { keyword ->
